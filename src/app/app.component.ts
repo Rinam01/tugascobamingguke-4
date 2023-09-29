faf73ba73c83293b53968ed8010d10f23fe0e82e
Rinam01 <inamrinam72@gmail.com>
2023-09-29 10:10:33 +0700

    minggu ke empat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  email = 'inamrinam72@gmail.com';
+  nama = 'Rinam';
+  nim = '212102430';
   title = 'AngularTheme';
 }
